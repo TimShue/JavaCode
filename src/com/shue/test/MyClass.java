81feffd8ada36f636ac01af3b2ce8d8653968e90
tim <tim@tim-x240>
2021-03-04 10:17:55 +0800

    The first commit

---
+++ src/com/shue/test/MyClass.java
@@ -0,0 +1,10 @@
+package com.shue.test;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("This is first java code for test GitHub!");
+	}
+
+}
